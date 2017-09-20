47c76c7023dc6b25b73dde0aa37440b4a99ed0d1
wh <1032398604@qq.com>
2017-09-20 11:03:12 +0800

    改为多线程

--- src/com/hh/main/Main.java
+++ src/com/hh/main/Main.java
@@ -1,18 +1,22 @@
 package com.hh.main;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import com.hh.collection.URLCollection;
 import com.hh.collection.URLEntity;
-import com.hh.sprider.WebSprider;
+import com.hh.sprider.WebSpider;
+import com.hh.sprider.parser.impl.HtmlParserOf80s;
+import com.hh.sprider.parser.impl.HtmlParserOfDYTT;
 import com.hh.util.XMLFileUtil;
 
 public class Main {
 	public static void main(String[] args) {
 		XMLFileUtil.readXml();
 		
-		WebSprider ws = new WebSprider();
-		ws.crawling();
+		new WebSpider(HtmlParserOfDYTT.class).start();
+		new WebSpider(HtmlParserOf80s.class).start();
 		
 		XMLFileUtil.writeXml(URLCollection.getInstance());
 		
--- src/com/hh/sprider/WebSpider.java
+++ src/com/hh/sprider/WebSpider.java
@@ -1,5 +1,8 @@
 package com.hh.sprider;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.hh.collection.URLCollection;
 import com.hh.collection.URLEntity;
 import com.hh.sprider.parser.HtmlParser;
@@ -10,31 +13,49 @@ import com.hh.sprider.parser.impl.HtmlParserOfDYTT;
  * @author hh
  * 2017-9-4 16:22:13
  */
-public class WebSprider {
-	private HtmlParser hp = new HtmlParserOfDYTT();
+public class WebSpider extends Thread{
+	private HtmlParser hp;
+	Map<Object, Integer> map = new HashMap<Object, Integer>();	// 用来记录每个ue对象循环次数
+	
+	public WebSpider(Class<? extends HtmlParser> objClass) {
+		try {
+			hp = objClass.newInstance();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+	}
 	
 	/**
 	 * 抓取过程
 	 */
 	public void crawling() {
-//		Map<Object, Integer> map = new HashMap<Object, Integer>();	// 用来记录每个ue对象循环次数
 		URLCollection urlC = URLCollection.getInstance();
 		while (!urlC.getUnVisitedEmpty()) {
 			URLEntity ue = urlC.removeUnVisited();
 			ue = hp.getDownUrl(ue);
 			if (ue.getDownURL() == null || "".equals(ue.getDownURL())) {
-				/*// 用来防止一直循环下去
+				// 用来防止一直循环下去
 				Integer count = map.get(ue);
 				if (count == null) {
-					urlC.addUnVisited(ue);
 					map.put(ue, 1);
 				} else if (count < 5) {
-					urlC.addUnVisited(ue);
 					map.put(ue, count+1);
-				}*/
+				} else {
+					continue ;
+				}
+
+				urlC.addUnVisited(ue);
 			} else {
 				urlC.addVisited(ue);
 			}
-		}
+		}		
+	}
+
+
+	@Override
+	public void run() {
+		crawling();
 	}
 }
