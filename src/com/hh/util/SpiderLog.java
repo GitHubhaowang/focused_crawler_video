6993b3afea1df2e0320ecffd6ee091c0914251cd
wh <1032398604@qq.com>
2017-10-09 14:41:19 +0800

    完善SpiderLog

--- src/com/hh/util/SpiderLog.java
+++ src/com/hh/util/SpiderLog.java
@@ -1,10 +1,19 @@
 package com.hh.util;
 
+import org.apache.log4j.Logger;
+
+import com.hh.collection.URLEntity;
+
 public class SpiderLog implements Observer {
+	public static Logger logger = Logger.getLogger("log4j.properties");
 
 	@Override
 	public void observer(Object obj, Object msg) {
-		
+		URLEntity ue = (URLEntity) msg;
+		if (ue.getDownURL() == null || ue.getDownURL() == "") {
+			logger.info("未查找到 - "+ue.getName()+" - ");
+		} else {
+			logger.info("已查找到 - "+ue.getName()+" - | 下载地址 ："+ue.getDownURL());
+		}
 	}
-	
 }
